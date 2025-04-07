fcac94c083a1a19d5744c95076d82da60500bdc6
Patrik <66878545+patrikkucerka97@users.noreply.github.com>
2025-04-07 16:10:08 +0200

    init pkg

---
+++ goodon.go
@@ -0,0 +1,63 @@
+package goodon
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func initTracer() func() {
+	ctx := context.Background()
+	// Create stdout exporter to see traces in the console
+	stdoutExporter, err := stdouttrace.New(
+		stdouttrace.WithPrettyPrint(),
+		stdouttrace.WithWriter(os.Stdout),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create stdout exporter: %v", err)
+	}
+
+	// Create OTLP exporter
+	otlpExporter, err := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithEndpoint("alloy:4317"), // FIX THIS
+		otlptracegrpc.WithInsecure(),             // FIX THIS
+	)
+	if err != nil {
+		log.Fatalf("Failed to create OTLP trace exporter: %v", err)
+	}
+
+	// Use both exporters
+	provider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(stdoutExporter),
+		sdktrace.WithBatcher(otlpExporter),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()), //decide on sampling
+	)
+
+	otel.SetTracerProvider(provider)
+
+	return func() {
+		ctx := context.Background()
+		if err := provider.Shutdown(ctx); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}
+}
+
+func init() {
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+	initTracer()
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
