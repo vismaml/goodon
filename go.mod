164d6ce3d2d23f280804403c2303918cb341d0c4
Patrik <66878545+patrikkucerka97@users.noreply.github.com>
2025-04-16 15:59:38 +0200

    add grpc handling

--- go.mod
+++ go.mod
@@ -7,27 +7,47 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.35.0
 	go.opentelemetry.io/otel/sdk v1.35.0
 	go.opentelemetry.io/otel/sdk/metric v1.35.0
+	go.uber.org/zap v1.18.1
+	google.golang.org/grpc v1.71.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/openzipkin/zipkin-go v0.2.0 // indirect
+	github.com/prometheus/client_golang v1.22.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250218202821-56aae31c358a // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
 
 require (
+	github.com/e-conomic/ctxtrace v1.0.3
+	github.com/e-conomic/ctxvml v0.0.5
+	github.com/e-conomic/zapvml v0.4.0
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.60.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.35.0
 	go.opentelemetry.io/otel/metric v1.35.0
 	go.opentelemetry.io/otel/trace v1.35.0
--- goodon.go
+++ goodon.go
@@ -3,8 +3,19 @@ package goodon
 import (
 	"context"
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/e-conomic/ctxtrace"
+	"github.com/e-conomic/ctxvml"
+	"github.com/e-conomic/zapvml"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
@@ -15,6 +26,9 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	api_health "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
@@ -22,19 +36,65 @@ var (
 	Meter  otelmetric.Meter
 )
 
+type GoodonConfig struct {
+	GRPCPort  string  `required:"true" split_words:"true"`
+	HostIp    string  `required:"true" split_words:"true"`
+	TraceRate float64 `required:"true" split_words:"true"`
+}
+
 const (
 	alloyGrpcPort         = "4317"
 	otelCollectorGrpcPort = "4317"
 )
 
+func StartWithDefaults(cfg GoodonConfig, serviceName string, healthServ api_health.HealthServer) error {
+	// Setup logging
+	zap.ReplaceGlobals(zapvml.Log)
+	grpc_zap.ReplaceGrpcLogger(zapvml.Log)
+
+	// Setup Goodon
+	shutdownTelemetry, err := StartTelemetryWithDefaults(serviceName, cfg.HostIp, cfg.TraceRate)
+	defer shutdownTelemetry()
+	if err != nil {
+		return err
+	}
+
+	// Setup gRPC server
+	opts := grpcOptions()
+	server := grpc.NewServer(opts...)
+	grpc_prometheus.EnableHandlingTimeHistogram()
+	grpc_prometheus.Register(server)
+
+	// Register: Health
+	api_health.RegisterHealthServer(server, healthServ)
+
+	// Graceful stop
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		zap.L().Info("graceful shutdown")
+		server.GracefulStop()
+	}()
+
+	// init
+	lis, err := net.Listen("tcp", ":"+cfg.GRPCPort)
+	if err != nil {
+		return err
+	}
+	res := server.Serve(lis)
+	_ = zapvml.Log.Sync()
+	return res
+}
+
 // StartTelemetryWihDefaults initializes OpenTelemetry with default settings
 func StartTelemetryWithDefaults(serviceName string, backendIp string, traceFreq float64) (func(), error) {
-	shutdownTracer, err := InitTracer(serviceName, backendIp, traceFreq)
+	shutdownTracer, err := initTracer(serviceName, backendIp, traceFreq)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize tracer: %w", err)
 	}
 
-	shutdownMeter, err := InitMeterProvider(serviceName, backendIp)
+	shutdownMeter, err := initMeterProvider(serviceName, backendIp)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize meter provider: %w", err)
 	}
@@ -45,8 +105,8 @@ func StartTelemetryWithDefaults(serviceName string, backendIp string, traceFreq 
 	}, nil
 }
 
-// InitTracer sets up the OpenTelemetry tracer provider with OTLP exporter
-func InitTracer(serviceName string, backendIp string, traceFreq float64) (func() error, error) {
+// initTracer sets up the OpenTelemetry tracer provider with OTLP exporter
+func initTracer(serviceName string, backendIp string, traceFreq float64) (func() error, error) {
 	ctx := context.Background()
 
 	// Create OTLP trace exporter
@@ -58,14 +118,10 @@ func InitTracer(serviceName string, backendIp string, traceFreq float64) (func()
 		return nil, fmt.Errorf("failed to create OTLP trace exporter %w", err)
 	}
 
-	resources, err := resource.New(
-		context.Background(),
-		resource.WithHost(),      // Host info
-		resource.WithContainer(), // Container info
-		resource.WithAttributes( // Manual attributes using semconv
-			semconv.ServiceName(serviceName),
-		),
-	)
+	resources, err := newResources(ctx, serviceName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create rtrace esources: %w", err)
+	}
 
 	provider := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(otlpExporter),
@@ -87,8 +143,8 @@ func InitTracer(serviceName string, backendIp string, traceFreq float64) (func()
 	}, nil
 }
 
-// InitMeterProvider sets up the OpenTelemetry meter provider with OTLP exporter
-func InitMeterProvider(serviceName string, backendIp string) (func() error, error) {
+// initMeterProvider sets up the OpenTelemetry meter provider with OTLP exporter
+func initMeterProvider(serviceName string, backendIp string) (func() error, error) {
 	ctx := context.Background()
 
 	// Create OTLP exporter
@@ -103,14 +159,10 @@ func InitMeterProvider(serviceName string, backendIp string) (func() error, erro
 	// Create metric reader with periodic export to the collector
 	reader := metric.NewPeriodicReader(otlpExporter, metric.WithInterval(60*time.Second))
 
-	resources, err := resource.New(
-		context.Background(),
-		resource.WithHost(),      // Host info
-		resource.WithContainer(), // Container info
-		resource.WithAttributes( // Manual attributes using semconv
-			semconv.ServiceName(serviceName),
-		),
-	)
+	resources, err := newResources(ctx, serviceName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create metric resources: %w", err)
+	}
 
 	// Create a new MeterProvider with the OTLP exporter
 	meterProvider := metric.NewMeterProvider(
@@ -139,4 +191,54 @@ func newPropagator() propagation.TextMapPropagator {
 		propagation.TraceContext{},
 		propagation.Baggage{},
 	)
+}
+
+// newResources creates a new resource with host and container information
+func newResources(ctx context.Context, serviceName string) (*resource.Resource, error) {
+	return resource.New(
+		ctx,
+		resource.WithHost(),      // Host info
+		resource.WithContainer(), // Container info
+		resource.WithAttributes( // Manual attributes using semconv
+			semconv.ServiceName(serviceName),
+		),
+	)
+}
+
+// grpcOptions returns the default goodon gRPC server options
+func grpcOptions() []grpc.ServerOption {
+	return []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(15 << 20),
+		grpc.ChainUnaryInterceptor(
+			grpc_prometheus.UnaryServerInterceptor,
+			grpc_ctxtags.UnaryServerInterceptor(
+				grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor),
+			),
+			ctxvml.UnaryServerInterceptor(),
+			ctxtrace.UnaryServerInterceptor(),
+			grpc_zap.UnaryServerInterceptor(zapvml.Log, grpc_zap.WithLevels(zapvml.CodeToLevel)),
+		),
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	}
+}
+
+// grpcWithAdditionalOptions returns the default goodon gRPC server options with additional options
+func grpcWithAdditionalOptions(additionalOptions func() []grpc.ServerOption) []grpc.ServerOption {
+	options := []grpc.ServerOption{
+		grpc.ChainUnaryInterceptor(
+			grpc_ctxtags.UnaryServerInterceptor(
+				grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor),
+			),
+			ctxvml.UnaryServerInterceptor(),
+			ctxtrace.UnaryServerInterceptor(),
+			grpc_zap.UnaryServerInterceptor(zapvml.Log, grpc_zap.WithLevels(zapvml.CodeToLevel)),
+		),
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	}
+
+	if additionalOptions != nil {
+		options = append(options, additionalOptions()...)
+	}
+
+	return options
 }
