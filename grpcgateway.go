4b66298f8d776001a66bfcb9fe59d69003cb2fe0
Patrik <66878545+patrikkucerka97@users.noreply.github.com>
2025-12-04 11:52:18 +0100

    add grpc gateway

---
+++ grpcgateway.go
@@ -0,0 +1,42 @@
+package goodon
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type RegisterFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error
+
+// StartHTTPGateway starts an HTTP gateway that proxies requests to the gRPC server.
+func StartHTTPGateway(grpcPort, httpPort string, registerFuncs ...RegisterFunc) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	grpcEndpoint := "localhost:" + grpcPort
+
+	for _, register := range registerFuncs {
+		if err := register(ctx, mux, grpcEndpoint, opts); err != nil {
+			return err
+		}
+	}
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	server := &http.Server{
+		Addr:         ":" + httpPort,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Minute,
+		WriteTimeout: 10 * time.Minute,
+		IdleTimeout:  10 * time.Minute,
+	}
+	zap.L().Info("Starting HTTP gateway", zap.String("port", httpPort), zap.String("grpc_endpoint", grpcEndpoint))
+	return server.ListenAndServe()
+}
