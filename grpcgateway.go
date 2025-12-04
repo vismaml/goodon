ca1db3bd4ff00c08a39d767a219fed26bd958fa6
Patrik <66878545+patrikkucerka97@users.noreply.github.com>
2025-12-04 15:43:08 +0100

    Add grpc gateway (#5)
    * add grpc gateway
    
    * ommit empty fields
    
    * remove cancel ctx
    
    * Update grpcgateway.go
    
    * Update grpcgateway.go
    
    * fix after comments
    
    * Update grpcgateway.go

---
+++ grpcgateway.go
@@ -0,0 +1,58 @@
+package goodon
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type RegisterFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error
+
+// StartHTTPGateway starts a gateway that proxies requests to translate HTTP/JSON requests into gRPC calls.
+// To start the gateway, call this function within a new goroutine and provide the gRPC functions to register.
+//
+// Example:
+//
+//	go func() {
+//		if err := goodon.StartHTTPGateway(cfg.GRPCPort, cfg.HTTPPort,
+//			annotatorpb.RegisterDocumentAnnotatorHandlerFromEndpoint,
+//		); err != nil && err != http.ErrServerClosed {
+//			log.Fatalf("failed to start HTTP gateway: %v", err)
+//		}
+//	}()
+func StartHTTPGateway(grpcPort, httpPort string, registerFuncs ...RegisterFunc) error {
+	ctx := context.Background()
+
+	mux := runtime.NewServeMux(
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				EmitUnpopulated: false, // this option omits fields with zero values
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true, // this option ignores unknown fields in the incoming JSON
+			},
+		}),
+	)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	grpcEndpoint := "localhost:" + grpcPort
+
+	for _, register := range registerFuncs {
+		if err := register(ctx, mux, grpcEndpoint, opts); err != nil {
+			return err
+		}
+	}
+
+	server := &http.Server{
+		Addr:         ":" + httpPort,
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+	return server.ListenAndServe()
+}
