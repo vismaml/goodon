c076553194db27d85ead8425eee4cf2e89345919
Patrik <66878545+patrikkucerka97@users.noreply.github.com>
2026-01-19 11:27:46 +0100

    Allow custom headers in the gateway (#6)
    * propagate  x-request-id
    
    * dd vml-username
    
    * Update grpcgateway.go
    
    * Update grpcgateway.go
    
    * Update grpcgateway.go
    
    * generalize documentation
    
    * Update grpcgateway.go

--- grpcgateway.go
+++ grpcgateway.go
@@ -3,6 +3,7 @@ package goodon
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -13,6 +14,15 @@ import (
 
 type RegisterFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error
 
+// customHeaderMatcher forwards the "x-request-id" and "vml-username" headers to the gRPC metadata.
+func customHeaderMatcher(key string) (string, bool) {
+	lowerKey := strings.ToLower(key)
+	if lowerKey == "x-request-id" || lowerKey == "vml-username" {
+		return lowerKey, true
+	}
+	return runtime.DefaultHeaderMatcher(key)
+}
+
 // StartHTTPGateway starts a gateway that proxies requests to translate HTTP/JSON requests into gRPC calls.
 // To start the gateway, call this function within a new goroutine and provide the gRPC functions to register.
 //
@@ -20,7 +30,7 @@ type RegisterFunc func(ctx context.Context, mux *runtime.ServeMux, endpoint stri
 //
 //	go func() {
 //		if err := goodon.StartHTTPGateway(cfg.GRPCPort, cfg.HTTPPort,
-//			annotatorpb.RegisterDocumentAnnotatorHandlerFromEndpoint,
+//			pb.RegisterHandlerFromEndpoint,
 //		); err != nil && err != http.ErrServerClosed {
 //			log.Fatalf("failed to start HTTP gateway: %v", err)
 //		}
@@ -37,6 +47,7 @@ func StartHTTPGateway(grpcPort, httpPort string, registerFuncs ...RegisterFunc) 
 				DiscardUnknown: true, // this option ignores unknown fields in the incoming JSON
 			},
 		}),
+		runtime.WithIncomingHeaderMatcher(customHeaderMatcher),
 	)
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	grpcEndpoint := "localhost:" + grpcPort
